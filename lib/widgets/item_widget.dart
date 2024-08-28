a88475a6512d63fc22e56546e13a72f98e97c65e
nobie30 <diggajbhiye5113@gmail.com>
2024-08-28 14:38:53 +0530

    Day 11 Completed

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$ ${item.price.toString()}",
+          textScaleFactor: 1.5,
+          style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
